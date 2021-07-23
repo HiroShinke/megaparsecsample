8401e4f010cbc90a1ea9b12a8258a39f31d2f584
Hirofumi SHINKE <hiro@molloy.local>
2021-07-23 13:13:34 +0900

    Comment out for the time being

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -21,55 +21,55 @@ import qualified AST
 
 type LLVMBuilder = IRBuilderT (ModuleBuilderT Identity)
 
-compile :: AST.Expr -> Text
-compile expr = ppllvm $ buildModule "main" $ mdo
-  form <- globalStringPtr "%d\n" "putNumForm"
-  printf <- externVarArgs "printf" [ptr i8] i32
-  foo <- function "foo" [(i32,"n")] i32 $ \[n] -> mdo
-    entry <- block `named` "entry"; mdo
-      call printf [(ConstantOperand form, []), (n, [])]
-      cmd <- icmp P.EQ (ConstantOperand (C.Int 32 0)) n
-      condBr cmd thenB elseB
-      thenB <- block `named` "thenB"; mdo
-        ret (int32 1)
-      elseB <- block `named` "elseB"; mdo
-        n' <- sub n (int32 1)
-        x  <- call foo [(n',[])]
-        r  <- mul n x
-        -- call printf [(ConstantOperand form, []), (r, [])]
-        ret r
-  function "main" [] i32 $ \[] -> mdo
-    entry <- block `named` "entry"; mdo
-      r <- toOperand expr
-      call printf [(ConstantOperand form, []), (r, [])]              
-      x <- call foo [(ConstantOperand (C.Int 32 10),[])]
-      call printf [(ConstantOperand form, []), (x, [])]              
-      ret (int32 0)
+-- compile :: AST.Expr -> Text
+-- compile expr = ppllvm $ buildModule "main" $ mdo
+--   form <- globalStringPtr "%d\n" "putNumForm"
+--   printf <- externVarArgs "printf" [ptr i8] i32
+--   foo <- function "foo" [(i32,"n")] i32 $ \[n] -> mdo
+--     entry <- block `named` "entry"; mdo
+--       call printf [(ConstantOperand form, []), (n, [])]
+--       cmd <- icmp P.EQ (ConstantOperand (C.Int 32 0)) n
+--       condBr cmd thenB elseB
+--       thenB <- block `named` "thenB"; mdo
+--         ret (int32 1)
+--       elseB <- block `named` "elseB"; mdo
+--         n' <- sub n (int32 1)
+--         x  <- call foo [(n',[])]
+--         r  <- mul n x
+--         -- call printf [(ConstantOperand form, []), (r, [])]
+--         ret r
+--   function "main" [] i32 $ \[] -> mdo
+--     entry <- block `named` "entry"; mdo
+--       r <- toOperand expr
+--       call printf [(ConstantOperand form, []), (r, [])]              
+--       x <- call foo [(ConstantOperand (C.Int 32 10),[])]
+--       call printf [(ConstantOperand form, []), (x, [])]              
+--       ret (int32 0)
 
-class LLVMOperand a where
-  toOperand :: a -> LLVMBuilder Operand
+-- class LLVMOperand a where
+--   toOperand :: a -> LLVMBuilder Operand
 
-instance LLVMOperand AST.Expr where
-  toOperand (AST.ExprTerm e) = toOperand e
-  toOperand (AST.Expr t AST.Add e) = mdo
-    t' <- toOperand t
-    e' <- toOperand e
-    add t' e'
-  toOperand (AST.Expr t AST.Sub e) = mdo
-    t' <- toOperand t
-    e' <- toOperand e
-    sub t' e'
+-- instance LLVMOperand AST.Expr where
+--   toOperand (AST.ExprTerm e) = toOperand e
+--   toOperand (AST.Expr t AST.Add e) = mdo
+--     t' <- toOperand t
+--     e' <- toOperand e
+--     add t' e'
+--   toOperand (AST.Expr t AST.Sub e) = mdo
+--     t' <- toOperand t
+--     e' <- toOperand e
+--     sub t' e'
 
-instance LLVMOperand AST.Term where
-  toOperand (AST.TermNumber n) = toOperand n
-  toOperand (AST.Term n AST.Mul t) = mdo
-    n' <- toOperand n
-    t' <- toOperand t
-    mul n' t'
-  toOperand (AST.Term n AST.Div t) = mdo
-    n' <- toOperand n
-    t' <- toOperand t
-    sdiv n' t'
+-- instance LLVMOperand AST.Term where
+--   toOperand (AST.TermNumber n) = toOperand n
+--   toOperand (AST.Term n AST.Mul t) = mdo
+--     n' <- toOperand n
+--     t' <- toOperand t
+--     mul n' t'
+--   toOperand (AST.Term n AST.Div t) = mdo
+--     n' <- toOperand n
+--     t' <- toOperand t
+--     sdiv n' t'
 
-instance LLVMOperand AST.Number where
-  toOperand (AST.Number n) = return (int32 n)
+-- instance LLVMOperand AST.Number where
+--   toOperand (AST.Number n) = return (int32 n)
--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,24 +1,25 @@
 
-module Interpreter (interprete) where
+module Interpreter where
+--module Interpreter (interprete) where
 
 import AST
 
-interprete :: Expr -> Integer
-interprete = eval
+-- interprete :: Expr -> Integer
+-- interprete = eval
 
-class Eval a where
-  eval :: a -> Integer
+-- class Eval a where
+--   eval :: a -> Integer
 
-instance Eval Expr where
-  eval (ExprTerm t) = eval t
-  eval (Expr t Add e) = (eval t) + (eval e)
-  eval (Expr t Sub e) = (eval t) - (eval e)
+-- instance Eval Expr where
+--   eval (ExprTerm t) = eval t
+--   eval (Expr t Add e) = (eval t) + (eval e)
+--   eval (Expr t Sub e) = (eval t) - (eval e)
 
-instance Eval Term where
-  eval (TermNumber n) = eval n
-  eval (Term n Mul t) = (eval n) * (eval t)
-  eval (Term n Div t) = (eval n) `div` (eval t)
-  eval (TermExpr e)   = eval e
-instance Eval Number where
-  eval (Number n) = n
+-- instance Eval Term where
+--   eval (TermNumber n) = eval n
+--   eval (Term n Mul t) = (eval n) * (eval t)
+--   eval (Term n Div t) = (eval n) `div` (eval t)
+--   eval (TermExpr e)   = eval e
+-- instance Eval Number where
+--   eval (Number n) = n
 
